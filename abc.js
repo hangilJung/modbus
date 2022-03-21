3141119f2a7b0db55d78f67f8589f8a82841770d
hangilJung <ever_g@naver.com>
2022-03-21 14:31:32 +0900

    last

---
+++ abc.js
@@ -0,0 +1,5 @@
+for (let i = 0; i < 100; i++) {
+  const rand1 = Math.random() * 100000;
+  const rand2 = Math.random() * 100000;
+  console.log(`(x,y),(${rand1}, ${rand2})`);
+}
---
+++ day.js
@@ -0,0 +1,3 @@
+const moment = require("moment");
+
+console.log(moment().subtract("days", 545).format("YYYY-MM-DD"));
